ace827dd171888db0bac899add1f412ae80e7e65
Denis Konchekov <dkonchekov@gmail.com>
2011-03-11 19:03:31 +0300

    Удалил отладочный вывод, ненужные закомментированные строки, добавил комментарий

--- amazon_smtp.py
+++ amazon_smtp.py
@@ -10,18 +10,12 @@ class AmazonSMTPServer(smtpd.SMTPServer):
     amazonSES = SESConnection()
 
     def process_message(self, peer, mailfrom, rcpttos, data):
-        #message = EmailMessage()
-        #message.subject = 'test'
-        #message.bodyText = data
         data = email.message_from_string(data)
+        # Amazon SES don't support 'Organization' header, which is created 
+        # by email_template module
         if 'Organization' in data:
             del data['Organization']
         data = data.as_string()
-        print "peer: ", peer
-        print "mailfrom: ", mailfrom
-        print "rcpttos: ", rcpttos
-        print "data: ", data
-
         result = self.amazonSES.send_raw_email(mailfrom, data, rcpttos)        
 
 server = AmazonSMTPServer(('127.0.0.1', 9090), None)
